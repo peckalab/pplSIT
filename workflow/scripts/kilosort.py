97f52753d537fc8da9159683855cf7f1585d7bb3
sobolev@bio.lmu.de <sobolev@gpu-xxl.epinet.bzm>
2024-12-13 16:24:18 +0100

    bugfixes to kilosort / units

--- workflow/scripts/kilosort.py
+++ workflow/scripts/kilosort.py
@@ -23,3 +23,7 @@ save_p = snakemake.config['kilosort']['save_preprocessed']
 # run kilosort
 ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
     run_kilosort(settings=settings, probe=probe, results_dir=results_dir, save_preprocessed_copy=save_p)
+
+# save configuration
+with open(os.path.join(results_dir, 'settings.json'), 'w') as f:
+    f.write(json.dumps(settings, indent=2))
