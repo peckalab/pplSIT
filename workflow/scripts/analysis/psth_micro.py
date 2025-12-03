2bb253c49742e8cb4896513160ada8e48c4f3e41
mbengala <miguel.bengala@campus.lmu.de>
2025-12-03 13:45:06 +0100

    Add conditional input handling for sound phase lock file in psth_bar and psth_micro

--- workflow/scripts/analysis/psth_micro.py
+++ workflow/scripts/analysis/psth_micro.py
@@ -15,7 +15,9 @@ from utils.maths import pval2text
 
 s_path  = os.path.dirname(snakemake.input[0])
 session = os.path.basename(s_path)
-sound_phase_lock_file = os.path.join(s_path, 'analysis', 'sound_phase_lock.h5')
+opt_files = snakemake.input.opt
+sound_phase_lock_file = opt_files[0] if opt_files else None
+# sound_phase_lock_file = os.path.join(s_path, 'analysis', 'sound_phase_lock.h5')
 
 # reading some configs
 with h5py.File(snakemake.input[0], 'r') as f:
@@ -99,7 +101,7 @@ for fig_id, stim_comb in enumerate(stim_comb_idxs):
         # if sound phase locking exists - plot with the label
         label1 = label_combs[fig_id][0]
         label2 = label_combs[fig_id][1]
-        if os.path.exists(sound_phase_lock_file):
+        if sound_phase_lock_file:
             with h5py.File(sound_phase_lock_file, 'r') as snd_f:
                 if label_combs[fig_id][0] in snd_f:
                     MRL = np.array(snd_f[label_combs[fig_id][0]][unit_name]['MRL_real'])
